76eba5313b5b0d7d68aea7df1f7fe97b02b74628
Kyoungil Lee <leekyoungil@gmail.com>
2015-04-01 00:54:46 +0900

    First commit

--- cachemem_server/src/main/java/com/leekyoungil/cachemem/memcached/MemcachedControl.java
+++ cachemem_server/src/main/java/com/leekyoungil/cachemem/memcached/MemcachedControl.java
@@ -14,137 +14,42 @@ import net.rubyeye.xmemcached.transcoders.SerializingTranscoder;
 import net.rubyeye.xmemcached.utils.AddrUtil;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Random;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 import java.util.Map.Entry;
 
 /**
  * Created by kyoungil_lee on 2014. 7. 24..
  */
-public class MemcachedControl implements MemcachedInfo {
-    private volatile static MemcachedControl instance = null;
+public class MemcachedControl extends MemcachedInfo {
 
-    private ConcurrentHashMap<Integer, String> serverNoMap = new ConcurrentHashMap<>();
+    /**
+     * define MemcachedControl singleton instance.
+     */
+    private volatile static MemcachedControl INSTANCE = null;
 
-    private CopyOnWriteArrayList<MemcachedClient> memcachedClient = new CopyOnWriteArrayList<>();
-    private MemcachedClient metaMemcachedClient = null;
-    private CopyOnWriteArrayList<Integer> confuseServerNo = new CopyOnWriteArrayList<>();
+    private MemcachedControl () { }
 
-    public MemcachedControl () { }
-
+    /**
+     * Gets instance.
+     *
+     * Singleton object.
+     *
+     * @return the instance
+     */
     public static MemcachedControl getInstance () {
-        if (instance == null) {
+        if (INSTANCE == null) {
             synchronized (MemcachedControl.class) {
-                if (instance == null) {
-                    instance = new MemcachedControl();
+                if (INSTANCE == null) {
+                    INSTANCE = new MemcachedControl();
                 }
             }
         }
 
-        return instance;
+        return INSTANCE;
     }
 
-    /**
-     * Connect boolean.
-     *
-     * memcached 와 접속 한다.
-     *
-     * @return the boolean
-     */
-    @Override
-    public synchronized boolean connect () {
-        if (this.metaMemcachedClient == null) {
-            metaConnect();
-        }
-
-        if (this.memcachedClient.size() > 0) {
-            return true;
-        }
-
-        for (String address : serverAddress) {
-            MemcachedClient tmpConnection = getMemcachedServerConnection(address);
-
-            if (tmpConnection != null) {
-                tmpConnection.setOpTimeout(5000L);
-                tmpConnection.setMergeFactor(50);
-                tmpConnection.setOptimizeMergeBuffer(false);
-                tmpConnection.setEnableHeartBeat(false);
-                tmpConnection.setOptimizeGet(false);
-
-                this.memcachedClient.add(tmpConnection);
-
-                this.serverNoMap.put(this.memcachedClient.size()-1, address);
-            }
-        }
-
-        return (this.memcachedClient.size() > 0) ? true : false;
-    }
-
-    private synchronized boolean metaConnect () {
-        if (this.metaMemcachedClient != null) {
-            return true;
-        }
-
-        this.metaMemcachedClient = getMemcachedServerConnection(metaServerAddress);
-
-        if (this.metaMemcachedClient != null) {
-            this.metaMemcachedClient.setOpTimeout(5000L);
-            this.metaMemcachedClient.setMergeFactor(50);
-            this.metaMemcachedClient.setOptimizeMergeBuffer(false);
-            this.metaMemcachedClient.setEnableHeartBeat(false);
-            this.metaMemcachedClient.setOptimizeGet(false);
-        }
-
-        return (this.metaMemcachedClient != null) ? true : false;
-    }
-
-    /**
-     * Gets memcached server connection.
-     *
-     * 서버댓수 만큼 접속 한다.
-     *
-     * @param serverAddress the server address
-     * @return the memcached server connection
-     */
-    @Override
-    public MemcachedClient getMemcachedServerConnection (String serverAddress) {
-        if (serverAddress.isEmpty()) {
-            return null;
-        }
-
-        try {
-            MemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses(serverAddress));
-            builder.setConnectionPoolSize(15);
-            builder.setConnectTimeout(5000);
-            builder.setCommandFactory(new BinaryCommandFactory());
-            builder.setSessionLocator(new ArrayMemcachedSessionLocator(net.rubyeye.xmemcached.HashAlgorithm.ONE_AT_A_TIME));
-            builder.getConfiguration().setSoTimeout(5000);
-            builder.getConfiguration().setWriteThreadCount(2);
-            builder.getConfiguration().setReadThreadCount(2);
-            builder.getConfiguration().setSessionIdleTimeout(10000);
-            builder.getConfiguration().setStatisticsServer(false);
-            builder.setSocketOption(StandardSocketOption.SO_RCVBUF, 10 * 1024 * 1024);
-            builder.setSocketOption(StandardSocketOption.SO_SNDBUF, 10 * 1024 * 1024);
-            builder.setSocketOption(StandardSocketOption.TCP_NODELAY, false);
-            builder.getConfiguration().setSessionReadBufferSize(10 * 1024 * 1024);
-            builder.setTranscoder(new SerializingTranscoder(10 * 1024 * 1024));
-            builder.getConfiguration().setStatisticsServer(false);
-
-            return (builder.build() != null) ? builder.build() : null;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    @Override
-    public boolean close() {
-        return true;
-    }
-
-    @Override
     public MemcachedResult set (String key, Object value, int ttl, String siteName, String ipAddress, String itemName, String originKey) {
         MemcachedResult memcachedResult = new MemcachedResult();
 
@@ -219,29 +124,16 @@ public class MemcachedControl implements MemcachedInfo {
             log.setItemName(itemName);
             log.setOriginKey(originKey);
 
-            CacheMem.logQueue.add(log);
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-            memcachedResult.setResult(false);
-            memcachedResult.setResultText(ERROR_002);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            memcachedResult.setResult(false);
-            memcachedResult.setResultText(ERROR_003);
-        } catch (MemcachedException e) {
-            e.printStackTrace();
-            memcachedResult.setResult(false);
-            memcachedResult.setResultText(ERROR_004);
-        } catch (Exception e) {
-            e.printStackTrace();
-            memcachedResult.setResult(false);
-            memcachedResult.setResultText(ERROR_005);
+            LinkedBlockingQueue<CacheMemLog> cacheMemsLog = CacheMem.LOG_QUEUE_MAP.get(CacheMem.QUEUE_NO);
+
+            cacheMemsLog.add(log);
+        } catch (TimeoutException|InterruptedException|MemcachedException e) {
+            memcachedResult = handleToException(e, memcachedResult);
         }
 
         return memcachedResult;
     }
 
-    @Override
     public MemcachedResult get (String key) {
         MemcachedResult memcachedResult = new MemcachedResult();
 
@@ -267,34 +159,18 @@ public class MemcachedControl implements MemcachedInfo {
                     checkMetaServer = true;
                     String flashDbAddress = (String) serverMetaAddress;
 
-                    for(Entry<Integer, String> entry : this.serverNoMap.entrySet()) {
-                        int keyNo = entry.getKey();
-                        String value = entry.getValue();
+                    Optional<Entry<Integer, String>> result = this.serverNoMap.entrySet().stream().filter(entry -> entry.getValue().equals(flashDbAddress)).findFirst();
 
-                        if (value.equals(flashDbAddress)) {
-                            tmpResult = this.memcachedClient.get(keyNo).get(key);
-                            break;
-                        }
+                    if (result.isPresent()) {
+                        tmpResult = this.memcachedClient.get(result.get().getKey()).get(key);
                     }
                 }
             } else {
                 this.metaMemcachedClient = null;
                 metaConnect();
             }
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-            memcachedResult.setResult(false);
-            memcachedResult.setResultText(ERROR_002);
-            tmpResult = null;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            memcachedResult.setResult(false);
-            memcachedResult.setResultText(ERROR_003);
-            tmpResult = null;
-        } catch (MemcachedException e) {
-            e.printStackTrace();
-            memcachedResult.setResult(false);
-            memcachedResult.setResultText(ERROR_004);
+        } catch (TimeoutException|InterruptedException|MemcachedException e) {
+            memcachedResult = handleToException(e, memcachedResult);
             tmpResult = null;
         }
 
@@ -309,20 +185,8 @@ public class MemcachedControl implements MemcachedInfo {
                             break;
                         }
                     }
-                } catch (TimeoutException e) {
-                    e.printStackTrace();
-                    memcachedResult.setResult(false);
-                    memcachedResult.setResultText(ERROR_002);
-                    tmpResult = null;
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    memcachedResult.setResult(false);
-                    memcachedResult.setResultText(ERROR_003);
-                    tmpResult = null;
-                } catch (MemcachedException e) {
-                    e.printStackTrace();
-                    memcachedResult.setResult(false);
-                    memcachedResult.setResultText(ERROR_004);
+                } catch (TimeoutException|InterruptedException|MemcachedException e) {
+                    memcachedResult = handleToException(e, memcachedResult);
                     tmpResult = null;
                 }
             }
@@ -341,7 +205,6 @@ public class MemcachedControl implements MemcachedInfo {
         return memcachedResult;
     }
 
-    @Override
     public MemcachedResult purge (String key) {
         MemcachedResult memcachedResult = new MemcachedResult();
 
@@ -359,18 +222,8 @@ public class MemcachedControl implements MemcachedInfo {
                 memcachedClientTmp.delete(key);
                 memcachedResult.setResultText(SUCCESS_001);
                 memcachedResult.setResult(true);
-            } catch (TimeoutException e) {
-                e.printStackTrace();
-                memcachedResult.setResult(false);
-                memcachedResult.setResultText(ERROR_002);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                memcachedResult.setResult(false);
-                memcachedResult.setResultText(ERROR_003);
-            } catch (MemcachedException e) {
-                e.printStackTrace();
-                memcachedResult.setResult(false);
-                memcachedResult.setResultText(ERROR_004);
+            } catch (TimeoutException|InterruptedException|MemcachedException e) {
+                memcachedResult = handleToException(e, memcachedResult);
             }
         }
 
@@ -378,93 +231,28 @@ public class MemcachedControl implements MemcachedInfo {
     }
 
     /**
-     * Connect check.
+     * Handle to exception.
      *
-     * memcached 접속 체크하며 접속에 문제가 있으면 1초 간격으로 3번 리트라잉을 시도한다.
-     *
-     * @param reTryCnt the re try cnt
-     * @return the boolean
+     * @param e the e
+     * @param memcachedResult the memcached result
+     * @return the memcached result
      */
-    private boolean connectCheck (int reTryCnt) {
-        if (this.memcachedClient.size() > 0) {
-            boolean connectionDestroy = false;
+    private MemcachedResult handleToException (Exception e, MemcachedResult memcachedResult) {
+        String errorType = e.toString().toLowerCase();
 
-            for (MemcachedClient memcachedClientTmp : this.memcachedClient) {
-                if (memcachedClientTmp.getAvailableServers().size() == 0) {
-                    connectionDestroy = true;
-                }
-            }
-
-            if (connectionDestroy) {
-                this.memcachedClient.parallelStream().forEach((memcachedClient) -> {
-                    try {
-                        memcachedClient.shutdown();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-
-                this.memcachedClient.clear();
-            }
-
-            return (!connectionDestroy) ? true : connectCheck(reTryCnt);
-        } else if (this.memcachedClient.size() == 0 && reTryCnt < 3) {
-            if (!connect()) {
-                reTryCnt++;
-
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                return connectCheck(reTryCnt);
-            }
-        } else  {
-            return false;
+        if (errorType.indexOf("timeoutexception") > -1) {
+            memcachedResult.setResultText(ERROR_002);
+        } else if (errorType.indexOf("interruptedexception") > -1) {
+            memcachedResult.setResultText(ERROR_003);
+        } else if (errorType.indexOf("memcachedexception") > -1) {
+            memcachedResult.setResultText(ERROR_004);
+        } else if (errorType.indexOf("exception") > -1) {
+            memcachedResult.setResultText(ERROR_005);
         }
 
-        return true;
-    }
+        memcachedResult.setResult(false);
+        e.printStackTrace();
 
-    /**
-     * Gets server no.
-     *
-     * @param inputNo the input no
-     * @return the server no
-     */
-    private int getServerNo (int inputNo, int oLoopCnt) {
-        if ((this.confuseServerNo.size() == this.memcachedClient.size()) || oLoopCnt >= 10) {
-            return -1;
-        }
-
-        // 접속 서버 번호 랜덤 생성
-        Random random = new Random();
-
-        int serverCount = this.memcachedClient.size();
-        int serverNo = (serverCount == 1) ? 0 : random.nextInt((serverCount));
-
-        if (inputNo >= 0 && serverCount > 1) {
-            int loopCnt = 0;
-            while (serverNo == inputNo || loopCnt < 30) {
-                serverNo = random.nextInt((serverCount));
-                loopCnt++;
-            }
-        } else if (inputNo >= 0 && serverCount <= 1) {
-            serverNo = -1;
-        }
-
-        boolean serverStatusCheck = true;
-
-        if (serverNo >= 0 && this.memcachedClient.get(serverNo).getAvailableServers().size() == 0) {
-            if (!this.confuseServerNo.contains(serverNo)) {
-                this.confuseServerNo.add(serverNo);
-            }
-
-            serverStatusCheck = false;
-            oLoopCnt++;
-        }
-
-        return (serverStatusCheck) ? serverNo : getServerNo(serverNo, oLoopCnt);
+        return memcachedResult;
     }
 }
--- cachemem_server/src/main/java/com/leekyoungil/cachemem/memcached/MemcachedInfo.java
+++ cachemem_server/src/main/java/com/leekyoungil/cachemem/memcached/MemcachedInfo.java
@@ -1,40 +1,256 @@
 package com.leekyoungil.cachemem.memcached;
 
+import com.google.code.yanf4j.core.impl.StandardSocketOption;
 import net.rubyeye.xmemcached.MemcachedClient;
+import net.rubyeye.xmemcached.MemcachedClientBuilder;
+import net.rubyeye.xmemcached.XMemcachedClientBuilder;
+import net.rubyeye.xmemcached.command.BinaryCommandFactory;
+import net.rubyeye.xmemcached.impl.ArrayMemcachedSessionLocator;
+import net.rubyeye.xmemcached.transcoders.SerializingTranscoder;
+import net.rubyeye.xmemcached.utils.AddrUtil;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
- * Created by kyoungil_lee on 2014. 7. 24..
+ * Created by Kyoungil_Lee on 3/31/15.
  */
-public interface MemcachedInfo {
-    // memcached run script
-    // ./memcached -p 11211 -d -u root -m 2045 -c 10240 -P /dev/shm/memcached.pid -t 3
+public abstract class MemcachedInfo implements MemcachedInterface {
 
-    public int timeout = 300;
+    /**
+     * memcached 서버의 주소 정보를 저장한다.
+     * save the server information. (memcached server ip address.)
+     */
+    protected ConcurrentHashMap<Integer, String> serverNoMap = new ConcurrentHashMap<>();
+    /**
+     * memcached 서버 접속 오브젝트를 저장한다.
+     * save the memcached server connect object. (included meta server.)
+     */
+    protected CopyOnWriteArrayList<MemcachedClient> memcachedClient = new CopyOnWriteArrayList<>();
+    protected MemcachedClient metaMemcachedClient = null;
+    /**
+     * memcached 서버중에 접속이 불가능한 서버 번호를 저장한다.
+     * save the server number can't connected in the memcached servers.
+     */
+    protected CopyOnWriteArrayList<Integer> confuseServerNo = new CopyOnWriteArrayList<>();
 
-    public String[] serverAddress = {"127.0.0.1:11211"};
+    /**
+     * Connect boolean.
+     *
+     * 정의한 서버의 수 만큼 memcached 서버 접속 object를 만든다.
+     * defined as the number of servers to create the memcached server connection object.
+     *
+     * @return the boolean
+     */
+    public synchronized boolean connect () {
+        /**
+         * 메타서버 접속 객체를 만든다.
+         * Create a meta server connect object.
+         */
+        if (this.metaMemcachedClient == null) {
+            metaConnect();
+        }
 
-    public String metaServerAddress = "127.0.0.1:11311";
+        if (this.memcachedClient.size() > 0) {
+            return true;
+        }
 
-    public final String SUCCESS_001 = "success";
-    public final String SUCCESS_002 = "empty";
-    public final String ERROR_001 = "flashDb connect exception";
-    public final String ERROR_002 = "timeout exception";
-    public final String ERROR_003 = "interrupted exception";
-    public final String ERROR_004 = "server exception";
-    public final String ERROR_005 = "known exception";
-    public final String ERROR_006 = "server has gone";
+        Arrays.asList(serverAddress).stream().parallel().forEach(address -> {
+            /**
+             * memcached 서버에 접속시도를 한다.
+             * try to connect to memcached server.
+             */
+            MemcachedClient tmpConnection = getMemcachedServerConnection(address);
 
-    public abstract boolean connect ();
+            if (tmpConnection != null) {
+                tmpConnection = setMemcachedClientOptions(tmpConnection);
+                this.memcachedClient.add(tmpConnection);
+                this.serverNoMap.put(this.memcachedClient.size()-1, address);
+            }
+        });
 
-    public abstract MemcachedClient getMemcachedServerConnection (String serverAddress);
+        return (this.memcachedClient.size() > 0) ? true : false;
+    }
 
-    public abstract boolean close ();
+    /**
+     * Meta connect.
+     *
+     * 메타서버 접속 object를 만든다.
+     *
+     * @return the boolean
+     */
+    public synchronized boolean metaConnect () {
+        if (this.metaMemcachedClient != null) {
+            return true;
+        }
 
-    public abstract MemcachedResult set (String key, Object value, int ttl, String siteName, String ipAddress, String itemName, String originKey);
+        /**
+         * 메타서버에 접속시도를 한다.
+         * try to connect to meta server.
+         */
+        this.metaMemcachedClient = getMemcachedServerConnection(metaServerAddress);
 
-    public abstract MemcachedResult get (String key);
+        if (this.metaMemcachedClient != null) {
+            this.metaMemcachedClient = setMemcachedClientOptions(this.metaMemcachedClient);
+        }
 
-    public abstract MemcachedResult purge (String key);
+        return (this.metaMemcachedClient != null) ? true : false;
+    }
 
 
+    /**
+     * Sets memcached client options.
+     *
+     * memcached 클라이언트 접속 옵션을 설정해 준다.
+     * set the memcached client connection options.
+     *
+     * @param tmpConnection the tmp connection
+     * @return the memcached client options
+     */
+    private MemcachedClient setMemcachedClientOptions (MemcachedClient tmpConnection) {
+        tmpConnection.setOpTimeout(5000L);
+        tmpConnection.setMergeFactor(50);
+        tmpConnection.setOptimizeMergeBuffer(false);
+        tmpConnection.setEnableHeartBeat(false);
+        tmpConnection.setOptimizeGet(false);
+
+        return tmpConnection;
+    }
+
+    /**
+     * Gets memcached server connection.
+     *
+     * @param serverAddress the server address
+     * @return the memcached server connection
+     */
+    @Override
+    public MemcachedClient getMemcachedServerConnection (String serverAddress) {
+        if (serverAddress.isEmpty()) {
+            return null;
+        }
+
+        try {
+            MemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses(serverAddress));
+            builder.setConnectionPoolSize(15);
+            builder.setConnectTimeout(5000);
+            builder.setCommandFactory(new BinaryCommandFactory());
+            builder.setSessionLocator(new ArrayMemcachedSessionLocator(net.rubyeye.xmemcached.HashAlgorithm.ONE_AT_A_TIME));
+            builder.getConfiguration().setSoTimeout(5000);
+            builder.getConfiguration().setWriteThreadCount(2);
+            builder.getConfiguration().setReadThreadCount(2);
+            builder.getConfiguration().setSessionIdleTimeout(10000);
+            builder.getConfiguration().setStatisticsServer(false);
+            builder.setSocketOption(StandardSocketOption.SO_RCVBUF, 10 * 1024 * 1024);
+            builder.setSocketOption(StandardSocketOption.SO_SNDBUF, 10 * 1024 * 1024);
+            builder.setSocketOption(StandardSocketOption.TCP_NODELAY, false);
+            builder.getConfiguration().setSessionReadBufferSize(10 * 1024 * 1024);
+            builder.setTranscoder(new SerializingTranscoder(10 * 1024 * 1024));
+            builder.getConfiguration().setStatisticsServer(false);
+
+            return (builder.build() != null) ? builder.build() : null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Connect check.
+     *
+     * memcached 접속 상태를 체크하며 접속에 문제가 있으면 1초 간격으로 3번 다시 시도한다.
+     * check the memcached connection status. if it have problem, at one second intervals three times to retry.
+     *
+     * @param reTryCnt the re try cnt
+     * @return the boolean
+     */
+    protected boolean connectCheck (int reTryCnt) {
+        if (this.memcachedClient.size() > 0) {
+            boolean connectionDestroy = false;
+
+            for (MemcachedClient memcachedClientTmp : this.memcachedClient) {
+                if (memcachedClientTmp.getAvailableServers().size() == 0) {
+                    connectionDestroy = true;
+                }
+            }
+
+            if (connectionDestroy) {
+                this.memcachedClient.parallelStream().forEach((memcachedClient) -> {
+                    try {
+                        memcachedClient.shutdown();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+
+                this.memcachedClient.clear();
+            }
+
+            return (!connectionDestroy) ? true : connectCheck(reTryCnt);
+        } else if (this.memcachedClient.size() == 0 && reTryCnt < 3) {
+            if (!connect()) {
+                reTryCnt++;
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                return connectCheck(reTryCnt);
+            }
+        } else  {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Gets server no.
+     *
+     * @param inputNo the input no
+     * @param oLoopCnt the o loop cnt
+     * @return the server no
+     */
+    protected int getServerNo (int inputNo, int oLoopCnt) {
+        if ((this.confuseServerNo.size() == this.memcachedClient.size()) || oLoopCnt >= 10) {
+            return -1;
+        }
+
+        /**
+         * 접속 서버 번호 랜덤 생성
+         * create server number by random.
+         */
+        Random random = new Random(System.nanoTime());
+
+        int serverCount = this.memcachedClient.size();
+        int serverNo = (serverCount == 1) ? 0 : random.nextInt((serverCount));
+
+        if (inputNo >= 0 && serverCount > 1) {
+            int loopCnt = 0;
+
+            while (serverNo == inputNo || loopCnt < 30) {
+                serverNo = random.nextInt((serverCount));
+                loopCnt++;
+            }
+        } else if (inputNo >= 0 && serverCount <= 1) {
+            serverNo = -1;
+        }
+
+        boolean serverStatusCheck = true;
+
+        if (serverNo >= 0 && this.memcachedClient.get(serverNo).getAvailableServers().size() == 0) {
+            if (!this.confuseServerNo.contains(serverNo)) {
+                this.confuseServerNo.add(serverNo);
+            }
+
+            serverStatusCheck = false;
+            oLoopCnt++;
+        }
+
+        return (serverStatusCheck) ? serverNo : getServerNo(serverNo, oLoopCnt);
+    }
 }
